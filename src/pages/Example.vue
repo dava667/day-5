bd7a0c316edb6581460c0a7288ca18c97ef42f9d
dava667 <daviddavtiah1@gmail.com>
2021-11-28 22:14:37 +0600

    day 5
    day 5

---
+++ src/pages/Example.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <h1>Example Page</h1>
+        <input type="text" v-model="Message">
+        <button class="btn btnPrimary" @click="SetMessage">Change Message</button>
+        <p> {{ getMessage }} </p>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+
